4065b9e9a8fbd7fa68c500a7f7393b05fe65d514
abdellatifLabr <abdellatif1898@gmail.com>
2021-02-06 13:51:46 +0100

    Add start date and end date

--- futures/schema/queries.py
+++ futures/schema/queries.py
@@ -1,10 +1,13 @@
+from datetime import timedelta
+
 import graphene
-from graphene_django.filter import DjangoFilterConnectionField
-from django.db.models.functions import Trunc
 from django.db.models import Avg
+from django.db.models.functions import Trunc
+from django.utils import timezone
+from graphene_django.filter import DjangoFilterConnectionField
 
 from ..models import Pair
-from .nodes import SpreadNode, FutureNode, PairNode
+from .nodes import FutureNode, PairNode, SpreadNode
 from .types import SpreadChartDataType
 
 
@@ -18,21 +21,22 @@ class SpreadChartQuery(graphene.ObjectType):
         SpreadChartDataType,
         pair_id=graphene.ID(required=True),
         time_frame=graphene.String(default_value='minute'),
-        count=graphene.Int(default_value=20)
+        start_date=graphene.DateTime(),
+        end_date=graphene.DateTime(default_value=timezone.now())
     )
 
-    def resolve_chart_spreads(self, info, pair_id, time_frame, count, **kwargs):
+    def resolve_chart_spreads(self, info, pair_id, time_frame, end_date, start_date=None, **kwargs):
         try:
             pair = Pair.objects.get(pk=pair_id)
         except Pair.DoesNotExist:
             raise Exception('This pair does not exist')
 
-        spreads = pair.spreads.all()
+        spreads = pair.spreads.filter(created_at__range=[start_date or end_date - timedelta(days=10), end_date])
         data = spreads.annotate(
             timestamp=Trunc('created_at', time_frame)).values('timestamp').annotate(
             buy_spread=Avg('buy_spread'),
             sell_spread=Avg('sell_spread')).values(
-            'buy_spread', 'sell_spread', 'timestamp').order_by('timestamp')[:count]
+            'buy_spread', 'sell_spread', 'timestamp').order_by('timestamp')
 
         return data
 
