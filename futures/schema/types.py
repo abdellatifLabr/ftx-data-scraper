71c91dcb7a8ae55471129d2e4b95e976f4fb3094
abdellatifLabr <abdellatif1898@gmail.com>
2021-02-06 14:00:12 +0100

    Remove required since fields are not input

--- futures/schema/types.py
+++ futures/schema/types.py
@@ -3,5 +3,5 @@ import graphene
 
 class SpreadChartDataType(graphene.ObjectType):
     timestamp = graphene.DateTime()
-    buy_spread = graphene.Float(required=True)
-    sell_spread = graphene.Float(required=True)
+    buy_spread = graphene.Float()
+    sell_spread = graphene.Float()
